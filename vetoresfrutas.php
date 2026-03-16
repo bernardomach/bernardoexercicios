89ae83fd647c342062e618d8a8558b7609e23bc0
bernardomach <bernardo.37029@aluno.iffar.edu.br>
2026-03-16 10:56:42 -0300

    Add files via upload

---
+++ vetoresfrutas.php
@@ -0,0 +1,10 @@
+<?php
+$vetor = ["Maçã", "Banana", "Laranja", "Uva", "Manga"];
+echo $vetor[0] . "<br>";
+echo $vetor[1] . "<br>";
+echo $vetor[2] . "<br>";
+echo $vetor[3] . "<br>";
+echo $vetor[4] . "<br>";
+$vetor[5] = "Abacaxi";
+
+?>
